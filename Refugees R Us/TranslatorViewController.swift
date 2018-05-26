3399115f8a83f839bc772447b8dbb733017f036f
jamestouri <j_touri@yahoo.com>
2018-05-26 16:05:18 -0700

    UI Picker selected

---
+++ Refugees R Us/TranslatorViewController.swift
@@ -0,0 +1,31 @@
+//
+//  TranslatorViewController.swift
+//  Refugees R Us
+//
+//  Created by James touri on 5/26/18.
+//  Copyright © 2018 jamestouri. All rights reserved.
+//
+
+import UIKit
+
+class TranslatorViewController: UIViewController {
+    
+    @IBOutlet weak var translateToLabel: UILabel!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBOutlet weak var translatePicker: UIPickerView!
+    
+   
+    
+}
--- Refugees R Us/ViewController.swift
+++ Refugees R Us/ViewController.swift
@@ -9,9 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    // Language that will be associated with the Google Translate API
+    var language_chosen: String?
+    
+ 
     override func viewDidLoad() {
         super.viewDidLoad()
+        language_select.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,7 +23,30 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    // Selection of Languages
+    @IBOutlet weak var language_select: UIPickerView!
+    
 
 
 }
 
+
+extension ViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return "This"
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        
+    }
+}
